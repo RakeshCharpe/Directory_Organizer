5c308a9a51a462ffa6b55768cbdd65b5d430ea54
Rakesh Charpe <69101908+RakeshCharpe@users.noreply.github.com>
2021-08-02 14:36:13 +0530

    Update pepOrganizer.js

--- pepOrganizer.js
+++ pepOrganizer.js
@@ -7,10 +7,10 @@ let inputArr = process.argv.slice(2);
 let command = inputArr[0];
 switch (command) {
   case "tree":
-    // node main.js tree "C:\Users\Ritik Singh\Desktop\Batches\PP_12_21\JS\Module_1_FS\randomFolder"
+   
     treeObj.treefn(inputArr[1]);
     break;
-  //  input -> node main.js organize "C:\Users\Ritik Singh\Desktop\Batches\PP_12_21\JS\Module_1_FS\randomFolder"
+
   case "organize":
     organizeObj.organizefn(inputArr[1]);
     break;
